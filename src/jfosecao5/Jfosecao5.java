1d8f9fa23ce9181b735465efdfc156d516585aad
Edilson do Nascimento <edilson.nascimento@gmail.com>
2020-10-05 14:52:54 -0300

    Inicio projeto

---
+++ src/jfosecao5/Jfosecao5.java
@@ -0,0 +1,15 @@
+package jfosecao5;
+/**
+ *
+ * @author Edilson do Nascimento
+ */
+public class Jfosecao5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
